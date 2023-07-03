a07f03704f1ec5039ff097d24122869d734f6f59
DESKTOP-GRUEFOB\koalr <koalrube@gmail.com>
2023-07-03 18:50:38 -0400

    added error pages

---
+++ mymixtape-app/src/app/about/error.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function Error(props: ErrorProps) {
+  const { error, reset } = props;
+
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="container py-5">
+      <h1>Error</h1>
+      <button type="button" onClick={() => reset()}>
+        Try again
+      </button>
+    </div>
+  );
+}
+
+interface ErrorProps {
+  error: Error;
+  reset: () => void;
+}
